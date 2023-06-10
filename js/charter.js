7a39baa057f1b69a57f21abe80ac9fa4285dd5f1
RajatPayghan <93655125+RajatPayghan@users.noreply.github.com>
2023-06-10 22:14:02 +0530

    🧹chore: Completed design of top two charts

--- js/charter.js
+++ js/charter.js
@@ -1,6 +1,8 @@
 // Utilities
 const dates = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31',];
-const factorOfChange = ['No Change', 'Appreciable Change', 'Marked Rise/Fall'];
+const factorOfChange = ['<=1', '>1 & <2', '>2 & <3', '>3'];
+
+
 
 
 
@@ -13,16 +15,51 @@ const factorOfChange = ['No Change', 'Appreciable Change', 'Marked Rise/Fall'];
     type: 'line',
     data: {
       labels: dates, // Chart x axis label
-      datasets: [{
-        label: 'Absolute',
-        data: [1, 19, 3, 5, 2, 3], // Data as array HERE
-        borderWidth: 1
-      }]
+      datasets: [
+        {
+          label: '1 Day',
+          data: [1, 19, 3, 5, 2, 3], // Data as array HERE
+          borderWidth: 1
+        },
+        {
+          label: '2 Day',
+          data: [1, 17, 3, 5, 2, 3], // Data as array HERE
+          borderWidth: 1
+        },
+        {
+          label: '3 Day',
+          data: [1, 19, 3, 8, 2, 3], // Data as array HERE
+          borderWidth: 1
+        },
+        {
+          label: '4 Day',
+          data: [1, 19, 6, 5, 2, 3], // Data as array HERE
+          borderWidth: 1
+        },
+        {
+          label: '5 Day',
+          data: [1, 19, 3, 5, 2, 1], // Data as array HERE
+          borderWidth: 1
+        }
+      ]
     },
     options: {
       scales: {
         y: {
           beginAtZero: true
+        }
+      },
+      animation: {
+        duration: 600
+      },
+      plugins: {
+        title: {
+            display: true,
+            text: 'Absolute Error',
+            padding: {
+                top: 0,
+                bottom: 10
+            }
         }
       }
     }
@@ -38,16 +75,50 @@ const factorOfChange = ['No Change', 'Appreciable Change', 'Marked Rise/Fall'];
     data: {
       labels: factorOfChange,
       datasets: [{
-        label: '# of Votes',
-        data: [13,12,6],
-        borderWidth: 1
-      }]
+          label: '1 Day',
+          data: [13,12,6], 
+          borderWidth: 1
+        },
+        { // Add more Datasets like this
+          label: '2 Day', // Your Label goes here
+          data: [9,11,12], // Your Data goes here
+          borderWidth: 2
+        },
+        { // Add more Datasets like this
+          label: '3 Day', // Your Label goes here
+          data: [9,11,12], // Your Data goes here
+          borderWidth: 2
+        },
+        { // Add more Datasets like this
+          label: '4 Day', // Your Label goes here
+          data: [9,11,12], // Your Data goes here
+          borderWidth: 2
+        },
+        { // Add more Datasets like this
+          label: '5 Day', // Your Label goes here
+          data: [9,11,12], // Your Data goes here
+          borderWidth: 2
+        }
+      ]
     },
     
     options: {
       scales: {
         y: {
           beginAtZero: true
+        }
+      },
+      animation: {
+        duration: 600
+      },
+      plugins: {
+        title: {
+            display: true,
+            text: 'Error Frequency in Range',
+            padding: {
+                top: 0,
+                bottom: 10
+            }
         }
       }
     }
