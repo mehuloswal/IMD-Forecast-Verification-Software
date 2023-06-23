3b409fa032441c04adc6f948edeef5e8440a64bd
RajatPayghan <93655125+RajatPayghan@users.noreply.github.com>
2023-06-23 22:36:30 +0530

    🔥feat: Added working special.html

--- js/resultTemp.js
+++ js/resultTemp.js
@@ -5,7 +5,7 @@ let inputObject = JSON.parse(localStorage.getItem("rainData"));
 !function(){
 
   if(inputObject != null){
-    document.getElementById('title-bar-output').innerText = `Rainfall Forecast Verification for ${inputObject.place}\n In the month of ${inputObject.month}, ${inputObject.year}`;
+    document.getElementById('title-bar-output').innerText = `Forecast Verification for ${inputObject.place}\n In the month of ${inputObject.month}, ${inputObject.year}`;
   }
   else {
     document.getElementById('title-bar-output').innerText = `You dont have any result data inputted`;
