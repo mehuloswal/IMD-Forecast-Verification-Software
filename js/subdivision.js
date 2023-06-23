89da2d1fde925e19230f233b211982919cf3bf8a
RajatPayghan <93655125+RajatPayghan@users.noreply.github.com>
2023-06-23 22:59:52 +0530

    🧹chore: Added CSS for subdivision

---
+++ js/subdivision.js
@@ -0,0 +1,39 @@
+const years = ['2023', '2022', '2021', '2020'];
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+const places = ['Pune', 'Mumbai', 'Satara'];
+const boolOptions = ['Yes', 'No'];
+
+// Initial Year Rendering in Select Button
+!function(){
+  let yearSelectors = '';
+
+  for (const key in years) {
+    yearSelectors += `<option value="${years[key]}">${years[key]}</option>`
+  }
+
+  document.querySelector('#year').innerHTML = yearSelectors;
+}();
+
+// Initial Month Rendering in Select Button
+!function(){
+  let monthSelectors = '';
+
+  for (const key in months) {
+    monthSelectors += `<option value="${months[key]}">${months[key]}</option>`
+  }
+
+  document.querySelector('#month').innerHTML = monthSelectors;
+  
+}();
+
+// Initial Place Rendering in Select Button
+!function(){
+  let placeSelectors = '';
+
+  for (const key in places) {
+    placeSelectors += `<option value="${places[key]}">${places[key]}</option>`
+  }
+
+  document.querySelector('#place').innerHTML = placeSelectors;
+  
+}();
