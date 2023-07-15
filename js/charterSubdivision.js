35ad80c0fcd1f47d1b65fa4088f476323a2ec75f
RajatPayghan <93655125+RajatPayghan@users.noreply.github.com>
2023-07-15 18:44:35 +0530

    🔥feat: Added CSI to subdivision

--- js/charterSubdivision.js
+++ js/charterSubdivision.js
@@ -47,6 +47,200 @@ console.log(output);
   });
 }();
 
+// CSI Dry
+!function(){
+  const ctx = document.getElementById('chart-csi-dry');
+  
+  new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: days, // Chart x axis label
+      datasets: [
+        {
+          label: 'PC',
+          data: [output.csi[0][0], output.csi[1][0],output.csi[2][0],output.csi[3][0],output.csi[4][0],],
+          borderWidth: 1
+        }
+      ]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      },
+      animation: {
+        duration: 600
+      },
+      plugins: {
+        title: {
+            display: true,
+            text: 'CSI Dry',
+            padding: {
+                top: 0,
+                bottom: 10
+            }
+        }
+      }
+    }
+  });
+}();
+
+// CSI Iso
+!function(){
+  const ctx = document.getElementById('chart-csi-iso');
+  
+  new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: days, // Chart x axis label
+      datasets: [
+        {
+          label: 'PC',
+          data: [output.csi[0][1], output.csi[1][1],output.csi[2][1],output.csi[3][1],output.csi[4][1],],
+          borderWidth: 1
+        }
+      ]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      },
+      animation: {
+        duration: 600
+      },
+      plugins: {
+        title: {
+            display: true,
+            text: 'CSI Isolated',
+            padding: {
+                top: 0,
+                bottom: 10
+            }
+        }
+      }
+    }
+  });
+}();
+
+//CSI Scat
+!function(){
+  const ctx = document.getElementById('chart-csi-scat');
+  
+  new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: days, // Chart x axis label
+      datasets: [
+        {
+          label: 'PC',
+          data: [output.csi[0][2], output.csi[1][2],output.csi[2][2],output.csi[3][2],output.csi[4][2],],
+          borderWidth: 1
+        }
+      ]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      },
+      animation: {
+        duration: 600
+      },
+      plugins: {
+        title: {
+            display: true,
+            text: 'CSI Scattered',
+            padding: {
+                top: 0,
+                bottom: 10
+            }
+        }
+      }
+    }
+  });
+}();
+
+// CSI Fairly Widespread
+!function(){
+  const ctx = document.getElementById('chart-csi-fw');
+  
+  new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: days, // Chart x axis label
+      datasets: [
+        {
+          label: 'PC',
+          data: [output.csi[0][3], output.csi[1][3],output.csi[2][3],output.csi[3][3],output.csi[4][3],],
+          borderWidth: 1
+        }
+      ]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      },
+      animation: {
+        duration: 600
+      },
+      plugins: {
+        title: {
+            display: true,
+            text: 'CSI Fairly Widespread',
+            padding: {
+                top: 0,
+                bottom: 10
+            }
+        }
+      }
+    }
+  });
+}();
+
+// CSI Widespread
+!function(){
+  const ctx = document.getElementById('chart-csi-wide');
+  
+  new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: days, // Chart x axis label
+      datasets: [
+        {
+          label: 'PC',
+          data: [output.csi[0][4], output.csi[1][4],output.csi[2][4],output.csi[3][4],output.csi[4][4],],
+          borderWidth: 1
+        }
+      ]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      },
+      animation: {
+        duration: 600
+      },
+      plugins: {
+        title: {
+            display: true,
+            text: 'CSI Widespread',
+            padding: {
+                top: 0,
+                bottom: 10
+            }
+        }
+      }
+    }
+  });
+}();
 
 // For HSS
 !function(){
